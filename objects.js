4d8f2c65e9d2f792bf71c937c9cd0e0e165774c9
Huy Pham <benjaminhuypham@gmail.com>
2019-08-12 20:36:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,9 @@ var playlist = {Taylor Swift: 'you belongs to me',
                 Adele: 'set fire to the rain',
                 Katy Perry: 'firework'
                }; 
+
+var updatePaylist = (playlist, artistName, songTitle) => {
+    return paylist = Object.assign(playlist, artistName:songTitle); 
+};
+
+updatePaylist();
